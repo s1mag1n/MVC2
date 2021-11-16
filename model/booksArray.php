42dc3459b5e19ce972b157763ddc51c97e461c58
s1mag1n <57657053+s1mag1n@users.noreply.github.com>
2021-11-16 08:23:15 +0200

    Add files via upload

---
+++ model/booksArray.php
@@ -0,0 +1,36 @@
+<?php
+$books = array(
+    array("bookname" => "HTML and CSS",
+        "author" => "Jon Duckett",
+        "price" => "17.19$",
+        "description" => "Every day, more and more people want to learn some HTML and CSS. Joining the professional web designers and programmers are new audiences who need to know a little bit of code at work",
+        "image" => "HTMLandCSS_Duckett.jpg"),
+
+    array("bookname" => "HTML5",
+        "author" => "Matthew MacDonald",
+        "price" => "27.65$",
+        "description" => "HTML is a collection of platform-independent styles (indicated by markup tags) that define the various components of a Web document.",
+        "image" => "HTML5TheMissing_MacDonald.jpg"),
+
+    array("bookname" => "Head First HTML5  Programming",
+        "author" => "Eric Freema, Elisabeth Robson",
+        "price" => "35.35$",
+        "description" => "HTML has been on a wild ride.sure,html started as a mere markup language,but more recently HTML's put on some major muscle.",
+        "image" => "HeadFirstHTML5.jpg"),
+
+    array("bookname" => "Head First JavScript Programming",
+        "author" => "Eric Freeman, Elisabet Robson",
+        "price" => "37.60$",
+        "description" => "Head First JavaScript Programming is a brain-friendly guide teaches you everything from JavaScript language fundamentals to advanced topics, including objects, functions, and the browser's document object model.",
+        "image" => "HeadFirstJavaScript.jpg"),
+
+    array("bookname" => "jQuery in Action",
+        "author" => "jQuery bibeault, Yehuda Katz",
+        "price" => "32.12$",
+        "description" => "A really good web development framework anticipates your needs. jQuery does more-it practically reads your mind. Developers fall in love with this JavaScript library the moment they see 20 lines of code reduced to three. jQuery is concise and readable.",
+        "image" => "JQueryInAction.jpg"));
+?>  
+    
+    
+
+
